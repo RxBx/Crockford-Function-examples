1a21efb693f7117b50d1a144fcfe1f04e8fa1037
RxBx <rxbabcock@yahoo.com>
2016-07-05 15:30:23 -0700

    clean and comment module examp
    Crockford page 40

--- module-example.js
+++ module-example.js
@@ -5,6 +5,7 @@ Function.prototype.method = function (name, func) {
 };
 
 String.method('deentityify', function() {
+	//data in entity is hidden inside the module & is inaccessible outside
 	var entity = {
 		quot: '"',
 		lt: '<',
@@ -12,12 +13,22 @@ String.method('deentityify', function() {
 	};
 
 	return function () {
-		return this.replace(/&([^&;]+);/g,
-			function (a,b) {
-				var r = entity[b];
-				return typeof r==='string' ? r: a;
+		return this.replace(/&([^&;]+);/g, //still learning regex syntax???, but seeks html entity items btwn '& .. ;''
+			//this function will supply the replacement string, based on comp match params w. entity storage items
+			function (a,b) { //a=found string; b= regex 'parenthesized submatch string' ([^&;]+)
+				var r = entity[b]; //accesses the entity object properties
+				return typeof r==='string' ? r : a; //r===string when property exists; otherwise, return w orig match 'a'
 			}
 		);
 	};
-}());//immediately invoked expression
+}());//immediately invoked expression which "seals" the data & mechanics of the deentityify method
 
+
+
+var hello = '"Hi < & bye >';
+
+console.log(hello.deentityify()); //"Hi < & bye >
+
+var bye = '&quot;&gt;&amp;&lt;'
+
+console.log(bye.deentityify()); //">&amp;<

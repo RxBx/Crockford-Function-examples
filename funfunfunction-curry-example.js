2270f73c2bac9df5b718a0851208a88bd862f66b
RxBx <rxbabcock@yahoo.com>
2016-07-18 10:57:31 -0700

    add library based curry transform

--- funfunfunction-curry-example.js
+++ funfunfunction-curry-example.js
@@ -20,3 +20,23 @@ let tinyDragon = fluffykinsDragon('tiny');
 
 console.log(tinyDragon('lightening')); //same output
 
+//vs standard functional form
+
+//which can be 'curried' via lodash library
+
+import _ from 'lodash'
+
+
+
+let dragon = (name, size, element) =>
+        name + 'is a ' +
+        size + ' dragon that breathes ' +
+        element + '!'
+
+dragon = _.curry(dragon);
+
+let fluffykinsDragon = dragon ('fluffykins');
+
+let tinyDragon = fluffykinsDragon('tiny');
+
+console.log(tinyDragon('lightening')); 
